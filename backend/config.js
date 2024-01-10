be3d2248481ed0898503533ef67497b58b4cfec8
Akhilesh Kr <2827daya@gmail.com>
2024-01-10 23:11:59 +0530

    Delete backend directory

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGO_URI:`mongodb+srv://sachinmahto:gLET912OqtHbCqXa@cluster0.pkws7l4.mongodb.net/FST?retryWrites=true&w=majority`
- }
- //mongodb+srv://sachinmahto:gLET912OqtHbCqXa@cluster0.pkws7l4.mongodb.net/?retryWrites=true&w=majority
